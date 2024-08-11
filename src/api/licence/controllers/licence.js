ad98cdb9b6a9fcc716f6bf647ab634c04e5e3088
mechack tambwe <mesutarso@users.noreply.github.com>
2024-08-11 14:28:21 +0100

    initial commit

---
+++ src/api/licence/controllers/licence.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * licence controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::licence.licence');
---
+++ src/api/licence/services/licence.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * licence service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::licence.licence');
